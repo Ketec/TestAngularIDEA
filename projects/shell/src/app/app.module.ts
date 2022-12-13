c9437cb6acd4a9a4121a0291cb83b1b6d1b2dab8
Mauro Tamm <mauro.tamm@fleetcomplete.eu>
2022-12-13 14:37:25 +0200

    init

--- projects/shell/src/app/app.module.ts
+++ projects/shell/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 // import { TestLibModule } from '../../../testlib/src/public-api';
-import { TestLibModule } from 'testlib';
+import { TestLibModule } from '@my/testlib';
 
 @NgModule({
   declarations: [
