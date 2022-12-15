2aff65dbba870b28edd5e84b77b9b5f4c4dcf858
Mauro Tamm <mauro.tamm@fleetcomplete.eu>
2022-12-15 12:18:30 +0200

    shell

--- projects/test-app/src/app/subdir/sub-comp/sub-comp.component.ts
+++ projects/test-app/src/app/subdir/sub-comp/sub-comp.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'test-sub-comp',
   templateUrl: './sub-comp.component.html',
+  styles: [':host { padding: 16px; display: block; }']
 })
 export class SubCompComponent {}
